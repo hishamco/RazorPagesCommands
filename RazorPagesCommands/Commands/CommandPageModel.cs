97f4a650717f4bab7977953e7a933f6b70f79320
hishamco <hishamco_2007@yahoo.com>
2017-07-08 08:32:46 +0300

    Add CommandPageModel

---
+++ RazorPagesCommands/Commands/CommandPageModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesCommands.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesCommands.Commands
+{
+    public abstract class CommandPageModel : PageModel
+    {
+        public Task<IActionResult> OnPostAsync()
+        {
+            var commandName = HttpContext.Request.Form[CommandTagHelper.CommandNameParam];
+            var commandArgs = Request.Query.ToDictionary(x => x.Key, x => x.Value.ToString(),
+                StringComparer.OrdinalIgnoreCase);
+
+            return InvokeCommandMethod(this, commandName, commandArgs) as Task<IActionResult>;
+        }
+
+        private static object InvokeCommandMethod(PageModel model, string name, IDictionary<string, string> args)
+        {
+            var commandMethod = model.GetType().GetMethod(name);
+            var commandMethodParams = commandMethod.GetParameters();
+            var @params = new List<object>();
+
+            if (commandMethodParams.Any())
+            {
+                for (int i = 0; i < commandMethodParams.Count(); i++)
+                {
+                    var param = commandMethodParams[i];
+
+                    if (args.ContainsKey(param.Name))
+                    {
+                        var formValue = args[param.Name].ToString();
+                        var value = Convert.ChangeType(formValue, param.ParameterType);
+                        @params.Add(value);
+                    }
+                }
+            }
+
+            return commandMethod.Invoke(model, @params.ToArray());       
+        }
+    }
+}
